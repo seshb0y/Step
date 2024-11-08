4feedb12f11bd53ef187b193ca6ab2bba09820f5
WildSedrick <seshb0y@mail.ru>
2024-11-08 13:45:34 +0300

    3

--- JS/homework/homework2.js
+++ JS/homework/homework2.js
@@ -70,33 +70,65 @@
 // те числа из диапазона, которые являются совершенными.
 // Используйте написанную ранее функцию, чтобы узнавать,
 // совершенное число или нет.
-function excellentNumInRange(a, b){
-    let res = 0;
-    for(let i = a+1; i <= b; i++){
-        for(let j = 0; j <= i / 2; i++){
-            if(i % j == 0)
-                
-        }
-        if(res == a)
-            {
-                return true;
-            }
-    }
+// function excellentNumInRange(a, b){
+//     let res = [];
 
-}
-alert(excellentNum(496));
+//     for(let i = a+1; i <= b; i++){
+//         if(excellentNum(i))
+//             {
+//                 res.push(i);
+//             }
+//     }
+//     console.log(res)
+
+// }
+// console.log(excellentNumInRange(1, 10000));
 
 // 7. Написать функцию, которая принимает время (часы, минуты, секунды) и выводит его на экран в формате «чч:мм:сс».
 // Если при вызове функции минуты и/или секунды не были
 // переданы, то выводить их как 00.
+// function dateTime(hours, min = "00", sec = "00"){
+//     console.log(hours,":", min, ":", sec);
+// }
+// console.log(dateTime(2, 42, 45));
+
 // 8. Написать функцию, которая принимает часы, минуты и
 // секунды и возвращает это время в секундах.
+// function intoSeconds(hours, min, sec){
+//     let secondsInHours = hours * 60 * 60;
+//     let secondsInMin = min * 60;
+//     return secondsInHours + secondsInMin + sec;
+// }
+// console.log(intoSeconds(5, 26, 36));
+
 // 9. Написать функцию, которая принимает количество секунд,
 // переводит их в часы, минуты и секунды и возвращает в
 // виде строки «чч:мм:сс».
+// function secConvert(seconds){
+//     let hours = Math.floor(seconds / 60 / 60);
+//     let min = Math.floor(seconds / 60 % 60);
+//     let sec = Math.floor(seconds % 60);
+//     console.log(hours,":",min,":",sec);
+// }
+// console.log(secConvert(65196));
 // 10. Написать функцию, которая считает разницу между датами.
 // Функция принимает 6 параметров, которые описывают 2
 // даты, и возвращает результат в виде строки «чч:мм:сс». При
 // выполнении задания используйте функции из предыдущих 2-х заданий: сначала обе даты переведите в секунды,
 // узнайте разницу в секундах, а потом разницу переведите
 // обратно в «чч:мм:сс».
+// function timeDifference(hours1, min1, sec1, hours2, min2, sec2)
+// {
+//     let firstTimeInSeconds = intoSeconds(hours1, min1, sec1);
+//     let secondTimeInSeconds = intoSeconds(hours2, min2, sec2);
+//     let dif;
+//     if(firstTimeInSeconds > secondTimeInSeconds)
+//     {
+//         dif = firstTimeInSeconds - secondTimeInSeconds;
+//     }
+//     else{
+//         dif = secondTimeInSeconds - firstTimeInSeconds;
+//     }
+//     secConvert(dif);
+// }
+// console.log(timeDifference(5, 24, 53, 15, 16, 47));
