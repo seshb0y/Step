b4eb7c272c54e798f0d2629a993f50cdd078ce91
WildSedrick <seshb0y@mail.ru>
2024-11-30 12:08:40 +0300

    1

--- JS/lessons/lesson9/dataBase.js
+++ JS/lessons/lesson9/dataBase.js
@@ -31,17 +31,43 @@
 const input = document.querySelector(".input");
 const btn = document.querySelector(".input__button");
 const ul = document.querySelector(".dataList");
-input.addEventListener("submit", () => {
-  localStorage.setItem("inputData", input.value);
-  input.value = "";
-  const li = document.createElement("li");
-  const localSettings = localStorage.getItem("inputData");
-  li.textContent = localSettings;
-  ul.appendChild(li);
-});
+
+// btn.addEventListener("click", () => {
+//   localStorage.setItem("inputData", input.value);
+//   input.value = "";
+//   const li = document.createElement("li");
+//   const localSettings = localStorage.getItem("inputData");
+//   li.textContent = localSettings;
+//   ul.appendChild(li);
+// });
 // btn.addEventListener("click", () => {
 //   for (let i = 0; i < ul.children.length; i += 1) {
 //     ul.children[i].removeChild();
 //   }
 //   localStorage.clear();
 // });
+
+
+btn.addEventListener("click", () => {
+  const li = document.createElement("li");
+  const localDataList = localStorage.getItem("inputData");
+  const listItems = localDataList ? JSON.parse(localDataList) : [];
+  listItems.push(input.value);
+  localStorage.setItem("inputData", JSON.stringify(listItems));
+  li.textContent = input.value;
+  ul.appendChild(li);
+  input.value = "";
+});
+function loadLocalData(){
+  const localDataList = localStorage.getItem("inputData");
+  if (localDataList) {
+    const listItems = JSON.parse(localDataList);
+    listItems.forEach((item) => {
+      const li = document.createElement("li");
+      li.textContent = item;
+      ul.appendChild(li); 
+    });
+  }
+}
+document.addEventListener("DOMContentLoaded", loadLocalData);
+
