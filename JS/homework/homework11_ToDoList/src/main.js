55befb2d48c725a7d3bad325379a123bd3e2f1c0
WildSedrick <seshb0y@mail.ru>
2024-11-30 21:01:03 +0300

    1

---
+++ JS/homework/homework11_ToDoList/src/main.js
@@ -0,0 +1,15 @@
+import './style.css'
+import { header } from './components/header'
+import { toDoList } from './components/toDoList'
+import { toDoForm } from './components/toDoForm'
+import { footer } from './components/footer'
+import { addTask } from './utilities/addTask'
+import { delTask } from './utilities/delTask'
+
+
+// localStorage.clear()
+header();
+toDoForm();
+toDoList();
+footer();
+addTask();
