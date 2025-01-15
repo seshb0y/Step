c09a1e5e39851bc4191856c1839f9edfccd0e143
seshb0y <seshb0yed@gmail.com>
2025-01-15 15:06:07 +0400

    1

--- React/homework/homework10-TS-Shopping/src/components/Navigation.tsx
+++ React/homework/homework10-TS-Shopping/src/components/Navigation.tsx
@@ -1,25 +1,22 @@
 import { NavLink, Route, Routes } from 'react-router-dom'
 import ShowCaseList from './ShowCaseList'
 import BasketList from './BasketList'
+import styles from './styles.module.css'
 
 const Navigation = () => {
   return (
-    <>
-        <nav>
-            <ul>
-                <NavLink to={'/'}>
-                    Show case
-                </NavLink>
-                <NavLink to={'/basket'}>
-                    Basket
-                </NavLink>
-            </ul>
-        </nav>
-        <Routes>
-            <Route path='/' element={<ShowCaseList/>}/>
-            <Route path='/basket' element={<BasketList/>}/>
-        </Routes>
-    </>
+    <div className={styles.container}>
+      <nav>
+        <ul>
+          <NavLink to={'/'}>Show case</NavLink>
+          <NavLink to={'/basket'}>Basket</NavLink>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<ShowCaseList />} />
+        <Route path='/basket' element={<BasketList />} />
+      </Routes>
+    </div>
   )
 }
 
--- React/homework/homework10-TS-Shopping/src/components/ShowCaseList.tsx
+++ React/homework/homework10-TS-Shopping/src/components/ShowCaseList.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { AppState } from '../redux/store'
-import { addProduct, deleteProduct, moveProduct } from '../redux/action';
+import { addProduct, deleteProduct, moveProduct } from '../redux/action'
+import styles from './styles.module.css'
 
 const ProductsList = () => {
-
     const [productId, setProductId] = useState(6)
-
     const [formData, setFormData] = useState({
         name: '',
         price: '',
@@ -15,12 +14,11 @@ const ProductsList = () => {
         amount: '',
     })
 
-    const dispatch = useDispatch();
-
-    const showCase = useSelector((state: AppState) => state.showCase);
+    const dispatch = useDispatch()
+    const showCase = useSelector((state: AppState) => state.showCase)
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
+        const { name, value } = e.target
         setFormData((prev) => ({
             ...prev,
             [name]: value,
@@ -28,62 +26,92 @@ const ProductsList = () => {
     }
 
     const handleAddProduct = (e: React.FormEvent) => {
-        e.preventDefault();
+        e.preventDefault()
         const product = {
             id: productId,
             ...formData,
             price: Number(formData.price),
             amount: Number(formData.amount),
-            status: false
+            status: false,
         }
         dispatch(addProduct(product))
-        setProductId(+1)
+        setProductId(productId + 1)
         setFormData({
             name: '',
             price: '',
             category: '',
             describe: '',
             amount: '',
-        });
+        })
     }
 
     const handleDeleteProduct = (id: number) => {
-        dispatch(deleteProduct({id}))
+        dispatch(deleteProduct({ id }))
     }
 
     const handleAddToBasket = (id: number, status: boolean) => {
-        dispatch(moveProduct({id, status: !status}))
+        dispatch(moveProduct({ id, status: !status }))
     }
 
-  return (
-    <div>
-        <ul>
-            {
-                showCase.filter((product) => product.amount >= 1).map((product) => (
+    return (
+        <div className={styles.container}>
+            <ul>
+                {showCase.filter((product) => product.amount >= 1).map((product) => (
                     <li key={product.id}>
-                        {product.name}
+                        <h3>{product.name}</h3>
                         <p>Price: {product.price}$</p>
                         <div>
                             <p>{product.category}</p>
                             <p>{product.describe}</p>
                             <p>Amount: {product.amount}</p>
                         </div>
-                        <button onClick={() => handleAddToBasket(product.id, product.status)} >Add to basket</button>
-                        <button onClick={()=>handleDeleteProduct(product.id)}>Delete product</button>
+                        <button onClick={() => handleAddToBasket(product.id, product.status)}>
+                            Add to basket
+                        </button>
+                        <button onClick={() => handleDeleteProduct(product.id)}>Delete product</button>
                     </li>
-                ))
-            }
-        </ul>
-        <form action="" onSubmit={handleAddProduct}>
-            <input name='name' type="text" value={formData.name} onChange={handleChange}  placeholder='name'/>
-            <input name='price' type="text" value={formData.price}  onChange={handleChange} placeholder='price'/>
-            <input name='category' type="text" value={formData.category}  onChange={handleChange} placeholder='category'/>
-            <input name='describe' type="text" value={formData.describe}  onChange={handleChange} placeholder='describe'/>
-            <input name='amount' type="text" value={formData.amount}  onChange={handleChange} placeholder='amount'/>
-            <button>Add</button>
-        </form>
-    </div>
-  )
+                ))}
+            </ul>
+            <form onSubmit={handleAddProduct}>
+                <input
+                    name="name"
+                    type="text"
+                    value={formData.name}
+                    onChange={handleChange}
+                    placeholder="name"
+                />
+                <input
+                    name="price"
+                    type="text"
+                    value={formData.price}
+                    onChange={handleChange}
+                    placeholder="price"
+                />
+                <input
+                    name="category"
+                    type="text"
+                    value={formData.category}
+                    onChange={handleChange}
+                    placeholder="category"
+                />
+                <input
+                    name="describe"
+                    type="text"
+                    value={formData.describe}
+                    onChange={handleChange}
+                    placeholder="describe"
+                />
+                <input
+                    name="amount"
+                    type="text"
+                    value={formData.amount}
+                    onChange={handleChange}
+                    placeholder="amount"
+                />
+                <button>Add</button>
+            </form>
+        </div>
+    )
 }
 
 export default ProductsList
