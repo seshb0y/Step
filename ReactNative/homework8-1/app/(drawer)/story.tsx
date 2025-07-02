ac6947127015fc77e9fe593dab283196d840e22c
seshb0y <seshb0yed@gmail.com>
2025-07-02 08:42:50 +0300

    02.07.2025

---
+++ ReactNative/homework8-1/app/(drawer)/story.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const story = () => {
+  return (
+    <View style={{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }}>
+      <Text>story</Text>
+    </View>
+  )
+}
+
+export default story
